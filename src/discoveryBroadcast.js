5a38c410d831b730b1f94c4a4d609341a88f0d3f
= <egodisho@ucsd.edu>
2019-03-10 05:44:15 -0700

    Fixed discovery broadcast.

--- src/discoveryBroadcast.js
+++ src/discoveryBroadcast.js
@@ -6,11 +6,12 @@ const MULTICAST_PORT = 3333;
 const BROADCAST_MSG = new Buffer('{"name":"HealthGlove", "port": 3000}');
 
 socket.bind(MULTICAST_PORT, () => {
+	socket.setBroadcast(true);
 	socket.addMembership(MULTICAST_IP);
 });
 
 setInterval(broadcast, 1000);
 
 function broadcast() {
-	socket.send(BROADCAST_MSG, MULTICAST_PORT);
+	socket.send(BROADCAST_MSG, MULTICAST_PORT, MULTICAST_IP);
 }
