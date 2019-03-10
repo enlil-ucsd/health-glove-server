e676a8054ce09fc73cb34324d0f1a8e9a7daee2f
Enlil Odisho <egodisho@ucsd.edu>
2019-03-10 11:02:53 +0000

    Added device discovery via MultiCast UDP

---
+++ src/discoveryBroadcast.js
@@ -0,0 +1,16 @@
+const dgram = require('dgram');
+const socket = dgram.createSocket('udp4');
+
+const MULTICAST_IP = "224.0.0.116";
+const MULTICAST_PORT = 3333;
+const BROADCAST_MSG = new Buffer('{"name":"HealthGlove", "port": 3000}');
+
+socket.bind(MULTICAST_PORT, () => {
+	socket.addMembership(MULTICAST_IP);
+});
+
+setInterval(broadcast, 1000);
+
+function broadcast() {
+	socket.send(BROADCAST_MSG, MULTICAST_PORT);
+}
